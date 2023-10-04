e2a3ed4b443037e01d7e99be1ea1051413a2d385
nhun0005 <142361107+nhun0005@users.noreply.github.com>
2023-10-04 13:39:51 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,69 @@
+var vis1 = "adel_wind_diag.vg.json";
+vegaEmbed("#adel_wind", vis1, {actions: false}).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis2 = "bris_wind_diag.vg.json";
+vegaEmbed("#bris_wind", vis2, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis3 = "hob_wind_diag.vg.json";
+vegaEmbed("#hob_wind", vis3, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis4 = "melb_wind_diag.vg.json";
+vegaEmbed("#melb_wind", vis4, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis5 = "perth_wind_diag.vg.json";
+vegaEmbed("#perth_wind", vis5, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis6 = "syd_wind_diag.vg.json";
+vegaEmbed("#syd_wind", vis6, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis7 = "as_wind_diag.vg.json";
+vegaEmbed("#as_wind", vis7, {actions: false}).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis8 = "ball_wind_diag.vg.json";
+vegaEmbed("#ball_wind", vis8, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis9 = "broome_wind_diag.vg.json";
+vegaEmbed("#broome_wind", vis9, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis10 = "canb_wind_diag.vg.json";
+vegaEmbed("#canb_wind", vis10, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis11 = "darw_wind_diag.vg.json";
+vegaEmbed("#darw_wind", vis11, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vis12 = "towns_wind_diag.vg.json";
+vegaEmbed("#towns_wind", vis12, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var specVis1 = "interactive_scatterplot.vg.json"
+vegaEmbed("#vis1", specVis1, { "actions": false }).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
+
+var specVis2 = "animated_map.vg.json"
+vegaEmbed("#vis2", specVis2, { "actions": false }).then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
